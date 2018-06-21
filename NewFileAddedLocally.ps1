9178c3a6d33eb156cdbbdbe75468c81e8fbb400b
Andy Helsby <21364480+Absoblogginlutely@users.noreply.github.com>
2018-06-21 16:00:40 -0400

    Updated comment in Newfileaddedlocally

--- NewFileAddedLocally.ps1
+++ NewFileAddedLocally.ps1
@@ -1,3 +1,3 @@
 Line 1 added to this file.
 Line 2 added
-#Also included a .gitnore and 2 pwd files that will not be synched.
+#Also included a .gitnore and 2 pwd files that will (or maybe it will!) not be synched.
