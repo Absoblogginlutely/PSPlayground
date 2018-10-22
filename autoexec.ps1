c1713140cfd9a50d6599655f4f5ff1b89e6323ab
Andy Helsby <21364480+Absoblogginlutely@users.noreply.github.com>
2018-10-22 08:12:11 -0400

    Added get-packt function

--- autoexec.ps1
+++ autoexec.ps1
@@ -1,7 +1,13 @@
+Function get-packt {
+    $Book = (Invoke-WebRequest -Uri https://www.packtpub.com/packt/offers/free-learning).ParsedHtml.getElementsByTagName('H2')[0].InnerHTML.Trim()
+    Write-Host 'The Packt Publishing free learning eBook of the day is: ' -ForegroundColor Cyan -NoNewline
+    Write-Host "'$Book'" -ForegroundColor Yellow
+    Write-host  "start https://www.packtpub.com/packt/offers/free-learning" -foregroundColor Cyan
+}
 if (test-path ("c:\temp\powershelllogs-" + $env:username + (get-date -uformat "%y%m%d") + "*.txt")) {$alreadyrun = $true}
 $transcriptlog = "c:\temp\powershelllogs-" + $env:username + (get-date -uformat "%y%m%d-%H%M") + ".txt"
 start-transcript $transcriptlog
- $host.ui.rawui.WindowTitle = $transcriptlog
+$host.ui.rawui.WindowTitle = $transcriptlog
 
 if (!$alreadyrun) {
     #Anything in this script block will only run once per day
